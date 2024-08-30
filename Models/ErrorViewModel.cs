9a7be2013fa01fe49da82ed0712b2eaf9e4361bf
gatekeeper321 <131513420+gatekeeper321@users.noreply.github.com>
2024-08-30 17:26:01 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10263164_CLDV6212_MCPETRIE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
